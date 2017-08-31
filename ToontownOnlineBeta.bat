d76646fe8dbaf5274106d42615810cb235883627
ronanwow1001 <ronancaleb123@gmail.com>
2017-08-31 09:55:31 -0700

    all fixed up

--- ToontownOnlineBeta.bat
+++ ToontownOnlineBeta.bat
@@ -2,7 +2,7 @@ set /P PYTHON=<PYTHON.txt
 %PYTHON% -m toontown.toonbase.ToontownStart
 DirectStart:starting the game...
 build version: 1.0
-Start OLD TT BETTER.exe
+Start toontown.exe
 [cmd done]
-URL:http%3A%2F%2Fwww.toontown.com%2Fdownload
+URL:http://toontown.com/download
 Press any key to continue...
